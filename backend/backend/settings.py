119a5d2194c3078455113b5d750c3d9b52976f08
Alexander Epov <sansan-3@mail.ru>
2026-02-28 12:32:40 +0300

    Fix flake8 issues: add docstrings and fix imports

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,20 +1,16 @@
-import os
-from dotenv import load_dotenv
 from pathlib import Path
+
+from dotenv import load_dotenv
 
 load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^'
 
 DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '123.123.123.123', '89.169.184.217']
-
-
-# Application definition
 
 INSTALLED_APPS = [
     'api.apps.ApiConfig',
@@ -59,35 +55,16 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'backend.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'django',                    # Имя БД
-        'USER': 'django_user',                # 👈 ВАЖНО: не postgres, а django_user
-        'PASSWORD': 'mysecretpassword',       # Пароль из .env
+        'NAME': 'django',
+        'USER': 'django_user',
+        'PASSWORD': 'mysecretpassword',
         'HOST': 'localhost',
         'PORT': '5432',
     }
 }
-#DATABASES = {
-    #'default': {
-        # Меняем настройку Django: теперь для работы будет использоваться
-        # бэкенд postgresql
-        #'ENGINE': 'django.db.backends.postgresql',
-        #'NAME': os.getenv('POSTGRES_DB', 'django'),
-        #'USER': os.getenv('POSTGRES_USER', 'django'),
-        #'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'mysecretpassword'),
-        #'HOST': os.getenv('DB_HOST', 'localhost'),
-        #'PORT': os.getenv('DB_PORT', 5432)
-    #}
-#}
-
-
-# Password validation
-# https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -104,30 +81,14 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
-# Internationalization
-# https://docs.djangoproject.com/en/3.2/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.2/howto/static-files/
-
 STATIC_URL = '/static/'
-
 STATIC_ROOT = BASE_DIR / 'collected_static'
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
